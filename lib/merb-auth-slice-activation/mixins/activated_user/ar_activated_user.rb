6b58bc1b73bc7adf6ab33b7160194543cfb76200
Rue the Ghetto <ru_ghetto@rubyghetto.com>
2009-02-27 17:42:15 -0800

    fix to ar_activated_user mixin

--- lib/merb-auth-slice-activation/mixins/activated_user/ar_activated_user.rb
+++ lib/merb-auth-slice-activation/mixins/activated_user/ar_activated_user.rb
@@ -5,14 +5,13 @@ module Merb
         module ARClassMethods
           def self.extended(base)
             base.class_eval do
-              before :create, :make_activation_code
-              after  :create, :send_signup_notification
+              before_create :make_activation_code
+              after_create :send_signup_notification
             end # base.class_eval
           
             def find_with_activation_code(ac)
               find(:first, :conditions => ["activation_code =?", ac])
             end
-          
           end # self.extended
         end # ARClassMethods
       end # ActivatedUser
